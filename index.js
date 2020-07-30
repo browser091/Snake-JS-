246c34a50411d980c5ae81e13896b0dc04b04339
pavel <browser091@gmail.com>
2020-07-30 17:15:44 +0300

    add snake

---
+++ index.js
@@ -0,0 +1,51 @@
+let field = document.createElement("div");
+document.body.appendChild(field);
+field.classList.add("field");
+
+for (let i = 0; i < 100; i++) {
+  let excel = document.createElement("div");
+  field.appendChild(excel);
+  excel.classList.add("excel");
+}
+
+let excel = document.getElementsByClassName("excel");
+let x = 1,
+  y = 10;
+
+for (let i = 0; i < excel.length; i++) {
+  if (x > 10) {
+    x = 1;
+    y--;
+  }
+  excel[i].setAttribute("posX", x);
+  excel[i].setAttribute("posY", y);
+  x++;
+}
+
+function generateSnake() {
+  let posX = Math.round(Math.random() * (10 - 3) + 3);
+  let posY = Math.round(Math.random() * (10 - 1) + 1);
+  return [posX, posY];
+}
+
+let coordinates = generateSnake();
+
+let snakeBody = [
+  document.querySelector(
+    `[posX =  + ${coordinates[0]} + ][posY = + ${coordinates[1]} + ]`
+  ),
+  document.querySelector(
+    "[posX ='" + coordinates[0] + "'][posY='" + (coordinates[1] - 1) + "']"
+  ),
+  document.querySelector(
+    "[posX ='" + coordinates[0] + "'][posY='" + (coordinates[1] - 2) + "']"
+  ),
+];
+
+for (let i = 0; i < snakeBody.length; i++) {
+  snakeBody[i].classList.add("snakeBody");
+}
+snakeBody[0].classList.add("snakeHead");
+
+console.log(coordinates);
+console.log(snakeBody);
